91b08f830e19509cbc602210d8da484032677da3
Rico Mutze <rico.mutze@trans4mation.de>
2024-10-15 17:12:27 +0200

    Temp checkin, adjusted base severity queries

--- app/Enums/AssessmentLifecycleStatus.php
+++ app/Enums/AssessmentLifecycleStatus.php
@@ -3,7 +3,12 @@ namespace App\Enums;
 
 use OpenApi\Attributes as OAT;
 
-#[OAT\Schema()]
+#[OAT\Schema(
+    schema: "AssessmentLifecycleStatus",  // Name des Schemas
+    type: "string",
+    enum: ["OPEN", "CLOSED"],  // Werte des Enums
+    description: "Status of the Assessment lifecycle"
+)]
 enum AssessmentLifecycleStatus: string {
     case OPEN = 'OPEN';
     case CLOSED = 'CLOSED';
